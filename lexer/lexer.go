84b5f418c05ba0c0ceade2e7363efe19c7040310
Youssef Zahi <youssef.zahi2016@gmail.com>
2023-12-13 10:26:51 +0100

    Create the lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,68 @@
+package lexer
+
+import (
+	"github.com/youzh00/Interpreter-in-go/token"
+)
+
+type Lexer struct {
+	// Current input that the lexer going to parse
+	input string
+	// Current position in the input => current char
+	position int
+	// current reading position in input => after current char
+	readPosition int
+	// current char under examination
+	ch rune
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+// goal is to give us the next character and advance our position in the input
+func (l *Lexer) readChar() {
+	if l.readPosition > len(l.input) {
+		// ASCII code for 'NUL'
+		l.ch = 0
+	}
+	l.ch = rune(l.input[l.readPosition])
+
+	l.position = l.readPosition
+	l.readPosition = l.position + 1
+
+}
+
+// We look at the current character under examination (l.ch) and return a token depending on which character it is
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch rune) token.Token {
+
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
