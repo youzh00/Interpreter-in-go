178d5e66ef489c1cd5d37e94c4fae54168c11b20
Youssef Zahi <youssef.zahi2016@gmail.com>
2023-12-13 17:06:25 +0100

    Support new keywords

--- token/token.go
+++ token/token.go
@@ -43,11 +43,21 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 //checks the keywords table to see whether the given identifier is in fact a keyword
