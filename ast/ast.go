8b94ea48e380b6c89e961efa0caf602805957aab
Youssef Zahi <youssef.zahi2016@gmail.com>
2023-12-16 11:13:45 +0100

    Create the program entry for AST

--- ast/ast.go
+++ ast/ast.go
@@ -11,3 +11,15 @@ type Expression interface {
 	Node
 	expressionNode()
 }
+
+// This is the node of every AST our parser produces
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
