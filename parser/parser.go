b0382596341ec6d75b1177bf7750efec372b5006
Youssef Zahi <youssef.zahi2016@gmail.com>
2024-01-11 19:27:17 +0100

    Create object interface and Integer object

--- parser/parser.go
+++ parser/parser.go
@@ -133,8 +133,7 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
-	// TODO: We're skipping the expressions until we
-	// encounter a semicolon
+
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
